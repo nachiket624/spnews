797571ba7e51340026843d33bfeb4afa2450467f
Nachiket Parjane <nachiketparjane054@gmail.com>
2023-04-16 09:20:30 +0530

    resolve website views render error

--- website/views.py
+++ website/views.py
@@ -1,57 +1,19 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User, auth
-from django.contrib.auth.decorators import login_required
-from website.views import index
-from adminpage.models import draft
-import datetime
+from django.shortcuts import render,get_object_or_404
+from adminpage.models import draft,herosection,herosectionnews
+# Create your views here.
+def index(request):
+    return render(request,"index.html",{"newnews":draft.objects.all(),"hero":herosection.objects.order_by('newstime')[0],"NHero":herosectionnews.objects.all()})
 
+def news(request,id) :
+    show = get_object_or_404(draft, pk=id)
+    return render(request, 'news.html',{'show':draft.objects.filter(id=id)})
 
-def login_view(request):
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username or password"}
-            return render(request, "masteradmin/login.html", context)
-        login(request, user)
-        return redirect('masteradmin/login.html')
-    return render(request, "masteradmin/login.html")
+def bigcard(request,id):
+    show = get_object_or_404(draft, pk=id)
+    return render(request, 'herosectionnews.html',{'hnews':herosection.objects.filter(id=id)})
+def herosectionnewscard(request,id):
+    show = get_object_or_404(draft, pk=id)
+    return render(request, 'livepage.html',{'hnewscard':herosectionnews.objects.filter(id=id)})
 
-
-@login_required(login_url='spadmin')
-def eDastboard(request):
-    return render(request, "masteradmin/editorDashboard.html")
-
-
-@login_required(login_url='spadmin')
-def eDraft(request):
-    return render(request, "masteradmin/editordraft.html",)
-
-
-def logout(request):
-    auth.logout(request)
-    return render(request, "index.html")
-
-
-@login_required(login_url='spadmin')
-def newDraft(request):
-    # return render(request, "masteradmin/editorDashboard.html")
-    print("Function Called")
-    if request.method == "POSt":
-        ToPage = request.POST.get("typepage")
-        Thumbnail = request.POST.get("myfile")
-        NewsTitle = request.POST.get("newstitle")
-        NewsTitlePara1 = request.POST.get("para1")
-        NewsTitlePara2 = request.POST.get("para2")
-        newstime = datetime.datetime.now()
-        newsauthor = "News Author"
-        status = "Draft"
-        print(ToPage,Thumbnail,NewsTitle,NewsTitlePara1,NewsTitlePara2)
-        post = draft(newspage=ToPage,newsthumbnail=Thumbnail,newstitle=NewsTitle,newspara1=NewsTitlePara1,newspara2=NewsTitlePara2,newstime=newstime,newsauthor=newsauthor,status=status)
-        post.save()
-        return render(request, "masteradmin/editorDashboard.html")
-    else:
-        print("something went worng")
+def commingsoon(request):
+    return render(request,'commingsoon.html')
